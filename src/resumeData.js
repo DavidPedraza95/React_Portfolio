a9e15f8752aff0265b4f3accbec599c1db79e172
DavidPedraza95 <82422713+DavidPedraza95@users.noreply.github.com>
2021-09-23 23:48:39 -0400

    initial files

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "David Pedraza",
+    "role": "Frontend Developer || Global Audit Internal Analyst",
+    "linkedinId":"https://www.linkedin.com/in/david-p-31114a68/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/david-p-31114a68/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/DavidPedraza95",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"404",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a current student in the UNCC Coding Bootcamp, as well as a Global Audit Internal Analyst at MUFG. My background includes eight years of Customer Service, five years of Financial Services, and three years of Wire Operations experience.",
+    "address":"4375 Timberwood Dr",
+    "website":"https://github.com/DavidPedraza95",
+    "education":[
+      {
+        "UniversityName":"UNCC",
+        "specialization":"Fullstack web Developing",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2021",
+        "Achievements":"Portfolio"
+      },
+      {
+        "UniversityName":"UNCC Belk College of Business",
+        "specialization":"Bachelor's of Business Administration concentration in Finance",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2020",
+        "Achievements":"Finance Degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"MUFG",
+        "specialization":"Global Internal Audit Analyst",
+        "MonthOfStarting":"September",
+        "YearOfLeaving":"-",
+        "Achievements":"Risk Management Audits across multiple lines of business"
+      },
+      {
+        "CompanyName":"Wells Fargo",
+        "specialization":"Wire Transfer Specialist",
+        "MonthOfLeaving":"Sept",
+        "YearOfLeaving":"2021",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Stock Tracker",
+        "description":"API Integration/HTML/CSS/JS",
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://davidpedraza95.github.io/Finance-Project-One/"
+      },
+      {
+        "name":"Enhanced Cyber-Security",
+        "description":"Secure Password Generation/Web Security",
+        "imgurl":"images/portfolio/project1.jpg",
+        "url":"https://davidpedraza95.github.io/Password-Generator/"
+      },
+      {
+        "name":"CDP Systems",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png",
+        "url": "https://protected-gorge-09109.herokuapp.com/"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"His intelligence is equal to mine, on all levels",
+        "name":"L"
+      },
+      {
+        "description":"A valuable asset to any team.",
+        "name":"Hideo Kojima"
+      },
+      {
+        "description":"If I wasn't the first, he would be.",
+        "name":"George Washington"
+      },
+      {
+        "description":"It takes a grand master to know a grand master.",
+        "name":"Ezio Auditore"
+      }
+    ]
+  }
+  
+  export default resumeData
